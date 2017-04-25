af4911af6e642cba54f3035759b75e9d039f4edb
wuhongxu <1102612827@qq.com>
2017-04-25 11:25:15 +0800

    修复商品图片不显示问题

--- src/components/Commodity/Commodity.js
+++ src/components/Commodity/Commodity.js
@@ -1,46 +1,51 @@
 import React from 'react'
 import './Commodity.scss'
-import {Carousel} from 'antd'
+import { Carousel } from 'antd'
 import TweenOne from 'rc-tween-one'
 
 export class Commodity extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor( props ) {
+    super( props )
     this.state = {
       current: 2,
-      show:false
+      show: false
     }
   }
-  handleClick(){
-    this.setState(Object.assign({},this.state,{show:!this.state.show}))
+  handleClick() {
+    this.setState( Object.assign( {}, this.state, {
+      show: !this.state.show
+    } ) )
   }
-  
+
   render() {
     const images = this.props.images
-    const banner = images.map((img, index) => (
-      <div key={this.props.title + index}>
-        <img src={img}/>
+    console.log( images )
+    const banner = images.map( ( img, index ) => (
+      <div className="img-container" key={this.props.title + index}>
+        <div>
+          <img src={img.url}/>
+        </div>
       </div>
-    ))
-    const animation = { left:-100, duration: 1000}
+    ) )
+    const animation = {
+      left: -100,
+      duration: 1000
+    }
     return (
-      <TweenOne animation={animation} paused={!this.state.show}>
-          <div className="com-item" onClick={this.handleClick.bind(this)}>
-            <Carousel
-              ref="commodity"
-              className="item-bg"
-              draggable
-              autoplay
-              infinite>
-              {banner}
-            </Carousel>
-            <div className="item-text">
-              <h3>{this.props.title}</h3>
-              <nobr>{this.props.description}</nobr>
-              <a className="my-link" href={this.props.url} target="_blank">点击购买&gt;&gt;</a>
-            </div>
-          </div>
-      </TweenOne>
+    <TweenOne animation={animation} paused={!this.state.show}>
+      <div className="com-item" onClick={this.handleClick.bind( this )}>
+        <Carousel ref="commodity" className="item-bg" draggable autoplay infinite>
+          {banner}
+        </Carousel>
+        <div className="item-text">
+          <h3>{this.props.title}</h3>
+          <nobr>
+            {this.props.description}
+          </nobr>
+          <a className="my-link" href={this.props.url} target="_blank">点击购买>></a>
+        </div>
+      </div>
+    </TweenOne>
     )
   }
 }
@@ -49,7 +54,6 @@ Commodity.propTypes = {
   title: React.PropTypes.string.isRequired,
   description: React.PropTypes.string.isRequired,
   images: React.PropTypes.array,
-  url:React.PropTypes.string.isRequired
+  url: React.PropTypes.string.isRequired
 }
-
 export default Commodity
