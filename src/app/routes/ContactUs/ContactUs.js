6dbdbef71b4942b36623996fee9bfbcc64fa7436
cc <1048831356@qq.com>
2017-11-04 14:38:59 +0800

    修改官方简介

--- src/app/routes/ContactUs/ContactUs.js
+++ src/app/routes/ContactUs/ContactUs.js
@@ -7,17 +7,16 @@ const ContactUs = () => (
     <h2>全国统一服务热线：400-886-6563</h2>
 
     <br/>
-    <h2>公司地址：成都市金牛区人民北路万达甲级写字楼B座702—704</h2>
+    <h2>公司邮箱：276316268@qq.com</h2>
+    <br/>
+    <h2>公司地址：成都市金牛区人民北路二段甲级写字楼B座702—704</h2>
+
+    <br/>
     <div className='text-center'>
       <img src="http://odp22tnw6.bkt.clouddn.com//v1/erweim.jpg" style={{maxWidth:'200px'}}/>
     </div>
     <h1 className='text-center'>微信公众号</h1>
-    <h1 className='text-center' style={{color:'#eee'}}>
-       我们在这
-    </h1>
-    <div style={{textAlign:'center',padding:'0 100px'}}>
-      <img style={{width:'100%'}} src='http://oow7renvm.bkt.clouddn.com/lianxiditu.jpg'/>
-    </div>
+
     <br/>
     
 
