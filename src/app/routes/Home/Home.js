180425dea78c3f3ea944767bc57ea5abd32ec535
cc <1048831356@qq.com>
2017-11-17 11:37:55 +0800

    替换首页banner

--- src/app/routes/Home/Home.js
+++ src/app/routes/Home/Home.js
@@ -54,7 +54,7 @@ export default class Home extends React.Component{
             <img src="http://odp22tnw6.bkt.clouddn.com/v1/commodity/banner8.jpg"/>
           </div>
           <div>
-            <img src="http://oow7renvm.bkt.clouddn.com/banner%E6%9C%AA%E4%BF%AE%E6%94%B91.jpg"/>
+            <img src="http://oow7renvm.bkt.clouddn.com/banner%E6%9C%AA%E4%BF%AE%E6%94%B92.jpg"/>
           </div>
         </Carousel>
         <div className="folder">
